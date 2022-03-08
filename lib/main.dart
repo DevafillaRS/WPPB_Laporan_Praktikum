71bc23e8e50b1b20d95c7c5bdf3350bd4aa41db1
DevafillaRS <73235055+DevafillaRS@users.noreply.github.com>
2022-03-08 20:01:01 +0700

    finish percobaan_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,30 @@
-import "package:flutter/material.dart";
-class DetailPage extends StatelessWidget {
-  const DetailPage({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'views/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Prak 3 Flutter Layout"),
-        ),
-        body: SafeArea(
-          child:
-          Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Image.asset('assets/images/kapal.jpg'),
-            Container(
-              margin: EdgeInsets.only(top: 16),
-              child: const Text(
-                "Surabaya Submarine Monument",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    child: Column(
-                      children: const [
-                        Icon(Icons.calendar_today),
-                        Text("Open Everyday"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: Column(
-                      children: const [
-                        Icon(Icons.access_time),
-                        Text("08:00 - 16:00"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: Column(
-                      children: const [
-                        Icon(Icons.attach_money),
-                        Text("Rp 10.000,-"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(16),
-              child: const Text(
-                "Museum inside a decommissioned Russian war submarine with tours & an adjacent park with cafes. Clean and well maintaned. Car park cost 10k, entrance fee 15k/person. You can see KRI Pasopati there, it is a russian whiskey class. You can also watch the video about the Indonesian Navy at the building beside the submarine",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            Container(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Image.network(
-                        "https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Image.asset('assets/images/kapal_2.JPG'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Image.asset('assets/images/kapal_3.jpeg'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Image.asset('assets/images/kapal_4.jpg'),
-                  ),
-                ],
-              ),
-            )
-          ]),
-        ));
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        fontFamily: "Inter",
+      ),
+      home: const MainScreen(),
+    );
   }
 }
